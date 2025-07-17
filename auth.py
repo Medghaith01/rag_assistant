20e8cff310ebf8c7d49a47317701ffca3d875237
Medghaith01 <med.ghaith@ieee.org>
2025-07-17 20:51:30 +0200

    Updated auth.py

--- auth.py
+++ auth.py
@@ -3,6 +3,6 @@ import os
 from fastapi import Header, HTTPException
 
 def verify_api_key(x_api_key: str = Header(...)):
-    expected_key = os.getenv("API_KEY")
+    expected_key = os.getenv("api_key")
     if x_api_key != expected_key:
         raise HTTPException(status_code=403, detail="Invalid or missing API Key")
